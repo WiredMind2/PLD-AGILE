6ef0b69bf39e1bedd5e6035bffd2d4339c122d43
William <william@michaud.re>
2025-10-06 11:48:18 +0200

    Added simple optimal algo

--- backend/app/utils/TSP/TSP.py
+++ backend/app/utils/TSP/TSP.py
@@ -1,6 +1,28 @@
+import heapq
 from .Astar import Astar
 
 
-class TSP(Astar):
+class TSP():
     def __init__(self):
-        super().__init__()
+        self.astar = Astar()
+
+    def solve(self, graph):
+        graph = self.astar.solve(graph)
+
+        start = list(graph.nodes)[0]
+        que = [(0, start, set())]
+        while que:
+            cost, node, visited = heapq.heappop(que)
+
+            for neighbor in graph.neighbors(node):
+                if neighbor not in visited:
+                    new_cost = cost + graph[node][neighbor]['weight']
+
+                    if len(visited) == len(graph.nodes) - 1 and neighbor == start:
+                        # Found a complete tour
+                        full_path = list(visited) + [start]
+                        return full_path, new_cost
+
+                    heapq.heappush(que, (new_cost, neighbor, visited | {neighbor}))
+        
+        return None, float('inf')
